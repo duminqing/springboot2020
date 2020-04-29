aeda57531ff0a46ffd99fce5c24e2eea480b830d
DMQ <duminqing@outlook.com>
2020-04-29 15:14:32 +0800

    spring boot redis redisson 测试
    Redis发布订阅在多机器部署时不能保证只有一个消费者消费
    在消费的时候如果有多个Listener会同步执行，而不是异步执行
    建议只一个MessageListener然后线程池执行不同任务

--- demo-redis/src/main/java/com/example/demoredis/StockController.java
+++ demo-redis/src/main/java/com/example/demoredis/StockController.java
@@ -1,29 +1,51 @@
 package com.example.demoredis;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.redisson.api.RLock;
+import org.redisson.api.RTopic;
 import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author Administrator
  */
+@Slf4j
 @RestController
 @RequestMapping("/stock")
 public class StockController {
     @Autowired
     private RedissonClient redissonClient;
 
-    @GetMapping("/stock")
+    @Autowired
+    private RedisTemplate<String, String> redisTemplate;
+
+    @GetMapping("/A")
     public void getA() {
+        log.info("hello stock");
         RLock lock = redissonClient.getLock("DMQ");
         try {
             lock.lock();
+
+            redisTemplate.opsForList().leftPush("DMQList", "测试1");
+            redisTemplate.opsForList().leftPush("DMQList", "测试2");
+            redisTemplate.opsForList().leftPush("DMQList", "测试3");
         } finally {
             lock.unlock();
         }
+    }
+
+    @GetMapping("/B/{stkId}")
+    public void getB(@PathVariable("stkId") String stkId) {
+        System.out.println("hehe");
+        RTopic topic = redissonClient.getTopic("Stock_My");
+        topic.publish(stkId);
+        System.out.println("heh  end");
+
     }
 }
