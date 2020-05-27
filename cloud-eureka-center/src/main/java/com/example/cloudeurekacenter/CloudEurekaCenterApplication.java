048e95ad1c7cb859c73400a188dcebcbb6ee5a33
duminqing <duminqing@outlook.com>
2020-05-27 22:55:02 +0800

    Spring Cloud eureka 注册中心+消费者和生产者

--- cloud-eureka-center/src/main/java/com/example/cloudeurekacenter/CloudEurekaCenterApplication.java
+++ cloud-eureka-center/src/main/java/com/example/cloudeurekacenter/CloudEurekaCenterApplication.java
@@ -1,16 +1,15 @@
-package com.example.cloudeurekaserver;
+package com.example.cloudeurekacenter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaClient
-public class CloudEurekaServerApplication {
+@EnableEurekaServer
+public class CloudEurekaCenterApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CloudEurekaServerApplication.class, args);
+        SpringApplication.run(CloudEurekaCenterApplication.class, args);
     }
 
 }
---
+++ cloud-eureka-center/src/test/java/com/example/cloudeurekacenter/CloudEurekaCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cloudeurekacenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudEurekaCenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
